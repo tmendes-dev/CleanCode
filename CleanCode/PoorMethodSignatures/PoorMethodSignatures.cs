09baa9a798de566997eb5c89a0bbe48b59ae39af
Thomas Mendes <th.mendes1997@gmail.com>
2021-05-03 21:18:06 -0300

    Poor methods signatures

--- CleanCode/PoorMethodSignatures/PoorMethodSignatures.cs
+++ CleanCode/PoorMethodSignatures/PoorMethodSignatures.cs
@@ -7,10 +7,10 @@ namespace CleanCode.PoorMethodSignatures
     {
         public void Run()
         {
-            var userService = new UserService();
+            UserService userService = new UserService();
 
-            var user = userService.GetUser("username", "password", true);
-            var anotherUser = userService.GetUser("username", null, false);
+            User user = userService.Login("username", "password");
+            User anotherUser = userService.GetUser("username");
         }
     }
 
@@ -18,25 +18,21 @@ namespace CleanCode.PoorMethodSignatures
     {
         private UserDbContext _dbContext = new UserDbContext();
 
-        public User GetUser(string username, string password, bool login)
+        public User GetUser(string username)
         {
-            if (login)
+            User user = _dbContext.Users.SingleOrDefault(u => u.Username == username);
+            return user;
+        }
+
+        public User Login(string username, string password)
+        {
+            User user = _dbContext.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+            if (user != null)
             {
-                // Check if there is a user with the given username and password in db
-                // If yes, set the last login date 
-                // and then return the user. 
-                var user = _dbContext.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
-                if (user != null)
-                    user.LastLogin = DateTime.Now;
-                return user;
+                user.LastLogin = DateTime.Now;
+                
             }
-            else
-            {
-                // Check if there is a user with the given username
-                // If yes, return it, otherwise return null
-                var user = _dbContext.Users.SingleOrDefault(u => u.Username == username);
-                return user;
-            }
+            return user;
         }
     }
 

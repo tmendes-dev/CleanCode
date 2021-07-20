0ed8fcbc223adf4340599a6dfd46196e0ccc5f25
Thomas Mendes <th.mendes1997@gmail.com>
2021-07-20 16:49:56 -0300

    Switch Statements - init

--- CleanCode.UnitTests/SwitchStatements/MonthlyStatementTests.cs
+++ CleanCode.UnitTests/SwitchStatements/MonthlyStatementTests.cs
@@ -12,7 +12,7 @@ namespace CleanCode.UnitTests.SwitchStatements
             var customer = new Customer { Type = CustomerType.PayAsYouGo };
             var usage = new MonthlyUsage { CallMinutes = 100, SmsCount = 100, Customer = customer };
             var statement = new MonthlyStatement();
-            statement.Generate(usage);
+            usage.Generate(statement);
 
             Assert.AreEqual(12.0f, statement.CallCost);
             Assert.AreEqual(12.0f, statement.SmsCost);
@@ -25,7 +25,7 @@ namespace CleanCode.UnitTests.SwitchStatements
             var customer = new Customer { Type = CustomerType.Unlimited };
             var usage = new MonthlyUsage { CallMinutes = 100, SmsCount = 100, Customer = customer };
             var statement = new MonthlyStatement();
-            statement.Generate(usage);
+            usage.Generate(statement);
 
             Assert.AreEqual(0, statement.CallCost);
             Assert.AreEqual(0, statement.SmsCost);
--- CleanCode/SwitchStatements/MonthlyUsage.cs
+++ CleanCode/SwitchStatements/MonthlyUsage.cs
@@ -1,9 +1,30 @@
-﻿namespace CleanCode.SwitchStatements
+﻿using System;
+
+namespace CleanCode.SwitchStatements
 {
     public class MonthlyUsage
     {
         public Customer Customer { get; set; }
         public int CallMinutes { get; set; }
         public int SmsCount { get; set; }
+
+        public void Generate(MonthlyStatement monthlyStatement)
+        {
+            switch (Customer.Type)
+            {
+                case CustomerType.PayAsYouGo:
+                    monthlyStatement.CallCost = 0.12f * CallMinutes;
+                    monthlyStatement.SmsCost = 0.12f * SmsCount;
+                    monthlyStatement.TotalCost = monthlyStatement.CallCost + monthlyStatement.SmsCost;
+                    break;
+
+                case CustomerType.Unlimited:
+                    monthlyStatement.TotalCost = 54.90f;
+                    break;
+
+                default:
+                    throw new NotSupportedException("The current customer type is not supported");
+            }
+        }
     }
 }

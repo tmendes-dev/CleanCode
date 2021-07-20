a3c248352f1c997dbff44103c92eb2bec9787a1b
Thomas Mendes <th.mendes1997@gmail.com>
2021-07-20 19:17:52 -0300

    Duplicated Code - Method to static, rename method to Parse, extracted method to time class

--- CleanCode/DuplicatedCode/DuplicatedCode.cs
+++ CleanCode/DuplicatedCode/DuplicatedCode.cs
@@ -4,26 +4,47 @@ namespace CleanCode.DuplicatedCode
 {
     internal class DuplicatedCode
     {
-       public class Time
+        public  class Time
         {
             public int Hours { get; set; }
             public int Minutes { get; set; }
+
             public Time(int hours, int minutes)
             {
                 Hours = hours;
                 Minutes = minutes;
             }
+            public static Time Parse(string timeString)
+            {
+                int time;
+                int hours;
+                int minutes;
+                if (!string.IsNullOrWhiteSpace(timeString))
+                {
+                    if (int.TryParse(timeString.Replace(":", ""), out time))
+                    {
+                        hours = time / 100;
+                        minutes = time % 100;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("timeString");
+                    }
+                }
+                else
+                    throw new ArgumentNullException("timeString");
+                return new Time(hours, minutes);
+            }
         }
+
         public void AdmitGuest(string name, string admissionDateTime)
         {
             // Some logic
             // ...
-            var time = GetTime(admissionDateTime);
-            var hours = time.Hours;
-            var minutes = time.Minutes;
+            var time = Time.Parse(admissionDateTime);
             // Some more logic
             // ...
-            if (hours < 10)
+            if (time.Hours < 10)
             {
             }
         }
@@ -33,37 +54,15 @@ namespace CleanCode.DuplicatedCode
             // Some logic
             // ...
 
-            var time = GetTime(admissionDateTime);
-            var hours = time.Hours;
-            var minutes = time.Minutes;
+            var time = Time.Parse(admissionDateTime);
 
             // Some more logic
             // ...
-            if (hours < 10)
+            if (time.Hours < 10)
             {
             }
         }
+       
 
-        private static Time GetTime( string admissionDateTime)
-        {
-            int time;
-            int hours = 0;
-            int minutes = 0;
-            if (!string.IsNullOrWhiteSpace(admissionDateTime))
-            {
-                if (int.TryParse(admissionDateTime.Replace(":", ""), out time))
-                {
-                    hours = time / 100;
-                    minutes = time % 100;
-                }
-                else
-                {
-                    throw new ArgumentException("admissionDateTime");
-                }
-            }
-            else
-                throw new ArgumentNullException("admissionDateTime");
-            return new Time(hours,minutes);
-        }
     }
 }

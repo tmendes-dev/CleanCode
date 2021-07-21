45815430ce94b5a951c64b48ace6ea53d5576442
Thomas Mendes <th.mendes1997@gmail.com>
2021-07-21 15:44:23 -0300

    Comments - Extract methods, removed bad comments, renamed variables

--- CleanCode/Comments/Comments.cs
+++ CleanCode/Comments/Comments.cs
@@ -6,7 +6,7 @@ namespace CleanCode.Comments
 {
     public class Comments
     {
-        private int _pf;  // pay frequency
+        private int _payFrequency;
         private DbContext _dbContext;
 
         public Comments()
@@ -14,27 +14,33 @@ namespace CleanCode.Comments
             _dbContext = new DbContext();
         }
 
-        // Returns list of customers in a country.
         public List<Customer> GetCustomers(int countryCode)
         {
-            //TODO: We need to get rid of abcd once we revisit this method. Currently, it's 
-            // creating a coupling betwen x and y and because of that we're not able to do 
-            // xyz. 
+            //TODO: We need to get rid of abcd once we revisit this method. Currently, it's
+            // creating a coupling betwen x and y and because of that we're not able to do
+            // xyz.
 
             throw new NotImplementedException();
         }
 
         public void SubmitOrder(Order order)
         {
-            // Save order to the database
+            SaveOrder(order);
+
+            NotifyCostumer(order);
+        }
+
+        private void SaveOrder(Order order)
+        {
             _dbContext.Orders.Add(order);
             _dbContext.SaveChanges();
+        }
 
-            // Send an email to the customer
+        private static void NotifyCostumer(Order order)
+        {
             var client = new SmtpClient();
             var message = new MailMessage("noreply@site.com", order.Customer.Email, "Your order was successfully placed.", ".");
             client.Send(message);
-
         }
     }
 
@@ -44,8 +50,6 @@ namespace CleanCode.Comments
 
         public void SaveChanges()
         {
-
-
         }
     }
 
@@ -53,10 +57,9 @@ namespace CleanCode.Comments
     {
         public void Add(Order order)
         {
-
-
         }
     }
+
     public class Order
     {
         public Customer Customer { get; set; }
